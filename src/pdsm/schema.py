3ca9c9a630b0459a82797cb19bd706de036c291e
Blake Imsland <blake@retroco.de>
2018-02-06 07:49:31 -0800

    Convert schema to columns without recursion

--- src/pdsm/schema.py
+++ src/pdsm/schema.py
@@ -34,7 +34,7 @@ def read_metadata(bucket, key, size):
     if size < (12 + footer_size):
         raise ParquetError('file is too small')
 
-    if magic_number != 'PAR1':
+    if magic_number != b'PAR1':
         raise ParquetError('magic number is invalid')
 
     offset = offset - footer_size
@@ -48,155 +48,104 @@ def read_metadata(bucket, key, size):
     return metadata
 
 
-class Node(object):
-    def __init__(self, data, child, sibling):
-        self.data = data
-        self.child = child
-        self.sibling = sibling
-
-    @property
-    def children(self):
-        children = []
-        child = self.child
-        while child:
-            children.append(child)
-            child = child.sibling
-        return children
-
-    @property
-    def name(self):
-        return self.data.name
-
-    @property
-    def type(self):
-        return self.data.type
-
-    @property
-    def converted_type(self):
-        return self.data.converted_type
-
-    @property
-    def repetition_type(self):
-        return self.data.repetition_type
-
-    @property
-    def num_children(self):
-        return self.data.num_children
-
-    @property
-    def precision(self):
-        return self.data.precision
-
-    @property
-    def scale(self):
-        return self.data.scale
-
-    def set_required(self):
-        self.data.repetition_type = 0
-
-    def is_group(self):
-        return self.type is None
-
-    def is_list(self):
-        return self.is_group() and self.converted_type == 3
-
-    def is_map(self):
-        return self.is_group() and self.converted_type in (1, 2)
-
-    def is_struct(self):
-        return self.is_group() and self.converted_type is None
-
-    def is_repeated(self):
-        return self.repetition_type == 2
-
-    def is_string(self):
-        # return self.type == 6 and self.converted_type == 0
-        return self.type == 6 and self.converted_type in (None, 0)
-
-    def is_decimal(self):
-        return self.type == 7 and self.converted_type == 5
-
-
 def to_columns(schema):
-    root = to_tree(schema)
-    columns = [Column(child.name.lower(), hive_type(child))
-               for child in root.children]
-    return columns
+    columns = []
+    context = []
 
+    column_name = ''
+    column_type = ''
 
-def to_tree(schema):
-    if not schema:
-        return None
+    # Set to "true" to skip the first element
+    skip_iteration = True
+    ignore_repetition = False
 
-    offset = 0
-    for idx, elem in enumerate(schema):
-        if elem.type is None:
-            offset += elem.num_children
-        if idx == offset:
-            break
+    for idx, element in enumerate(schema):
+        # Skip iteration if skip was set
+        if skip_iteration:
+            skip_iteration = False
+            continue
 
-    data = schema[0]
-    left = schema[1:offset+1]
-    right = schema[offset+1:]
-    return Node(data, to_tree(left), to_tree(right))
-
-
-def hive_type(node):
-    converted = None
-
-    # list type
-    if node.is_list():
-        child = node.child
-        # if the repeated field is not a group, than its type is the element type and elements are
-        # required
-        if not child.is_group():
-            child.set_required()
-            converted = 'array<{}>'.format(hive_type(child))
-
-        # if the repeated field is a group with multiple fields, than its type is the element type
-        # and elements are required
-        elif child.is_group() and child.num_children > 1:
-            child.set_required()
-            converted = 'array<{}>'.format(hive_type(child))
-
-        # if the repeated field is a group with one field and is named either array or uses the
-        # LIST-annotated group's name with _tuple appended then the repeated type is the element
-        # type and elements are required
-        elif (child.is_group() and child.num_children == 1
-              and child.name in ('array', node.name + '_tuple')):
-            child.set_required()
-            converted = 'array<{}>'.format(hive_type(child))
-
+        # If there's no current context, set current
+        if len(context) == 0:
+            column_name = element.name.lower()
+            column_type = ''
         else:
-            converted = 'array<{}>'.format(hive_type(child.child))
+            # If we're in a group but not the first member, add a comma
+            if column_type[-1] != '<':
+                column_type += ','
 
-    # map type
-    elif node.is_map():
-        child = node.child.child
-        key = hive_type(child)
-        val = hive_type(child.sibling)
-        converted = 'map<{},{}>'.format(key, val)
+            # If we're in a struct, append the name to the type
+            if context[-1][0] == 0:
+                column_type += element.name.lower() + ':'
 
-    # struct type
-    elif node.is_struct():
-        subs = ['{}:{}'.format(child.name, hive_type(child)) for child in node.children]
-        converted = 'struct<{}>'.format(','.join(subs))
+            # Decrement context
+            context[-1][1] -= 1
 
-    # unannotated repeated type
-    elif node.is_repeated():
-        node.set_required()
-        converted = 'array<{}>'.format(hive_type(node))
+        # Group Type
+        if element.type is None:
+            # List Type
+            if element.converted_type == 3:
+                child = schema[idx+1]
 
-    # byte_array type + utf8 converted_type = string
-    elif node.is_string():
-        converted = 'string'
+                # If child is not a group, or has more than 1 child, or is
+                # named "array" or "<parent.name>_tuple", than ignore the
+                # "repetition type" of the next element
+                if (child.type is not None or child.num_children > 1
+                        or child.name in ('array', element.name + '_tuple')):
+                    ignore_repetition = True
 
-    # decimal type
-    elif node.is_decimal():
-        converted = 'decimal({},{})'.format(node.precision, node.scale)
+                # Otherwise skip the next element
+                skip_iteration = not ignore_repetition
 
-    # conversion map
-    elif node.type in TYPE_MAP:
-        converted = TYPE_MAP[node.type]
+                context.append([2, 1])
+                column_type += 'array<'
 
-    return converted
+            # Map Type
+            elif element.converted_type in (1, 2):
+                # Always skip next element
+                skip_iteration = True
+                context.append([1, 2])
+                column_type += 'map<'
+
+            # Struct Type
+            else:
+                context.append([0, element.num_children])
+                column_type += 'struct<'
+
+            # Skip rest of iteration
+            continue
+
+        # List Type (Unannotated Repeated)
+        if element.repetition_type == 2:
+            if not ignore_repetition:
+                context.append([2, 0])
+                column_type += 'array<'
+            else:
+                ignore_repetition = False
+
+        # String Type
+        if element.type == 6 and element.converted_type in (None, 0):
+            column_type += 'string'
+
+        # Decimal Type
+        elif element.type == 7 and element.converted_type == 5:
+            column_type += 'decimal({},{})'.format(element.precision, element.scale)
+
+        # Simple Type
+        elif element.type in TYPE_MAP:
+            column_type += TYPE_MAP[element.type]
+
+        # Unknown Type
+        else:
+            raise ParquetError('unknown element type {}'.format(element))
+
+        # Unwind context until empty or it's last count is > 0
+        while len(context) > 0 and context[-1][1] == 0:
+            column_type += '>'
+            context.pop()
+
+        # If context is empty, we're back at root
+        if len(context) == 0:
+            columns.append(Column(column_name, column_type))
+
+    return columns
